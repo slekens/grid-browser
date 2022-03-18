a35a2067f0736bece34ff9e116db830a5e0045fd
Abraham Abreu <slek_2001@hotmail.com>
2022-03-18 15:02:33 -0600

    First Commit, project setup

---
+++ GridBrowser/GridBrowser/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GridBrowser
+//
+//  Created by Abraham Abreu on 18/03/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
